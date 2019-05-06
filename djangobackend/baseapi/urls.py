183da50d5a9699662a1061a75960b6dd59f979c7
Andrew <dybov.andrew@gmail.com>
2019-05-06 22:44:43 +0500

    Remove format_suffix_patterns to avoid suffix duplications

--- djangobackend/baseapi/urls.py
+++ djangobackend/baseapi/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-from rest_framework.urlpatterns import format_suffix_patterns
-
 from . import views
 
 
@@ -14,5 +12,3 @@ urlpatterns = [
         name='react-paths',
     ),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
